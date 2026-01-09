3c073fc74cc13cf0e9cd3ad45e8aa7dde2f82456
Julian Sanchez <julianwsan@gmail.com>
2026-01-09 13:56:54 -0500

    printing more pieces

--- src/ChessBoard.cpp
+++ src/ChessBoard.cpp
@@ -3,6 +3,8 @@
 #include "ChessBoard.h"
 #include "Piece.h"
 #include "Pawn.h"
+#include "Rook.h"
+#include "Knight.h"
 
 ChessBoard::ChessBoard(){
   for (int row = 0; row < 8; row++){
@@ -11,7 +13,21 @@ ChessBoard::ChessBoard(){
     }
   }
 
-  board[1][0] = new Pawn(BLACK);
+  // Pawns
+  for (int col = 0; col < 8; col++){ board[1][col] = new Pawn(BLACK); }
+  for (int col = 0; col < 8; col++){ board[6][col] = new Pawn(WHITE); }
+
+  // Rooks
+  board[0][0] = new Rook(BLACK);
+  board[0][7] = new Rook(BLACK);
+  board[7][0] = new Rook(WHITE);
+  board[7][7] = new Rook(WHITE);
+
+  // Knight
+  board[0][1] = new Knight(BLACK);
+  board[0][6] = new Knight(BLACK);
+  board[7][1] = new Knight(WHITE);
+  board[7][6] = new Knight(WHITE);
 }
 
 void ChessBoard::printBoard() const{
