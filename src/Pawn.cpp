8e3cf5120cd31bdfe3ec797f6e59a9e4bc6b7684
Julian Sanchez <julianwsan@gmail.com>
2026-01-18 18:30:04 -0500

    added basic pawn movement

--- src/Pawn.cpp
+++ src/Pawn.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <climits>
 #include "Pawn.h"
 #include "ChessBoard.h"
 
@@ -10,8 +11,33 @@ Pawn::Pawn(Color color){
 
 void Pawn::move(ChessBoard& board, int fromRow, int fromCol, int toRow, int toCol, Color color){
   Piece* piece = board.get(fromRow, fromCol);
+  int startingRow = 6;
+
+  int rowDiff = std::abs(fromRow - toRow);
+  int colDiff = std::abs(fromCol - toCol);
+
   if (piece->getPieceType() == PAWN){
-    board.set(fromRow, fromCol, toRow, toCol, piece);
+
+    // Moving one one space forward
+    if (rowDiff == 1 && colDiff == 0 && board.get(toRow, toCol) == nullptr){
+      board.set(fromRow, fromCol, toRow, toCol, piece);
+      return;
+    }
+
+    // Moving two spaces forward if on the initial spot
+    if (fromRow == startingRow && rowDiff == 2){
+      board.set(fromRow, fromCol, toRow, toCol, piece);
+      return;
+    }
+
+    // Moving diagonally if there is a piece that can be taken
+    if (rowDiff == 1 && colDiff == 1 && board.get(toRow, toCol) != nullptr){
+      board.set(fromRow, fromCol, toRow, toCol, piece);
+      return;
+    }
+
+    std::cout << "You made an illegal Pawn move!" << std::endl;
+    
   }
   else{
     std::cout << "You have selected an incorrect piece.";
