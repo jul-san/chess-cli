f367dd07a299a478c9b0df817fe7cd699fcfd72e
Julian Sanchez <julianwsan@gmail.com>
2026-05-10 19:17:47 -0400

    created knight movement logic

--- src/Knight.cpp
+++ src/Knight.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
+#include <cstdlib>
 #include "Knight.h"
 #include "ChessBoard.h"
 
 Knight::Knight(Color color){
   pieceColor = color;
-  type = PAWN;
+  type = KNIGHT;
   icon = (color == WHITE) ? "♞ " : "♘ ";
 }
 
 void Knight::move(ChessBoard& board, char fromCol, int fromRow, char toCol, int toRow, Color color){
-  std::cout << "In Knight CPP" << std::endl;
+  int colDiff = std::abs(fromCol - toCol);
+  int rowDiff = std::abs(fromRow - toRow);
+
+  bool validShape = (colDiff == 1 && rowDiff == 2) || (colDiff == 2 && rowDiff == 1);
+  if (!validShape){
+    std::cout << "You made an illegal Knight move!" << std::endl;
+    return;
+  }
+
+  Piece* dest = board.get(toCol, toRow);
+  if (dest != nullptr && dest->getPieceColor() == color){
+    std::cout << "You made an illegal Knight move!" << std::endl;
+    return;
+  }
+
+  board.set(fromCol, fromRow, toCol, toRow, this);
 }
 
 PieceType Knight::getPieceType() const{
