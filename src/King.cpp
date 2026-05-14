89a2cb687995b573576f0e7571133e5574e8f8f2
Julian Sanchez <julianwsan@gmail.com>
2026-05-13 21:30:27 -0400

    king movement logic added

--- src/King.cpp
+++ src/King.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <format>
+#include <cmath>
 #include "King.h"
 #include "ChessBoard.h"
 
@@ -9,8 +11,24 @@ King::King(Color color){
 }
 
 bool King::move(ChessBoard& board, char fromCol, int fromRow, char toCol, int toRow, Color color){
-  std::cout << "In King CPP" << std::endl;
-  return false;
+  int colDiff = std::abs(toCol - fromCol);
+  int rowDiff = std::abs(toRow - fromRow);
+
+  if (colDiff > 1 || rowDiff > 1 || (colDiff == 0 && rowDiff == 0)) {
+    std::cout << std::format("Illegal move: {}{} to {}{} is not a valid King move.\n",
+      fromCol, fromRow, toCol, toRow);
+    return false;
+  }
+
+  Piece* dest = board.get(toCol, toRow);
+  if (dest != nullptr && dest->getPieceColor() == color) {
+    std::cout << std::format("Illegal move: {}{} to {}{} is occupied by a friendly piece.\n",
+      fromCol, fromRow, toCol, toRow);
+    return false;
+  }
+
+  board.set(fromCol, fromRow, toCol, toRow, board.get(fromCol, fromRow));
+  return true;
 }
 
 PieceType King::getPieceType() const{
