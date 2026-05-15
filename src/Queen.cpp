0539dcabc6703bbb241c32788e60832ff6e81b29
Julian Sanchez <julianwsan@gmail.com>
2026-05-14 23:17:49 -0400

    queen movement logic

--- src/Queen.cpp
+++ src/Queen.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <format>
+#include <cmath>
 #include "Queen.h"
 #include <string>
 #include "ChessBoard.h"
@@ -10,8 +12,42 @@ Queen::Queen(Color color){
 }
 
 bool Queen::move(ChessBoard& board, char fromCol, int fromRow, char toCol, int toRow, Color color) {
-  std::cout << "In Quenn CPP" << std::endl;
-  return false;
+  int colDiff = std::abs(toCol - fromCol);
+  int rowDiff = std::abs(toRow - fromRow);
+
+  bool isDiagonal = colDiff == rowDiff && colDiff != 0;
+  bool isStraight = (fromCol == toCol) != (fromRow == toRow);
+
+  if (!isDiagonal && !isStraight) {
+    std::cout << std::format("Illegal move: {}{} to {}{} is not a valid Queen move.\n",
+      fromCol, fromRow, toCol, toRow);
+    return false;
+  }
+
+  Piece* dest = board.get(toCol, toRow);
+  if (dest != nullptr && dest->getPieceColor() == color) {
+    std::cout << std::format("Illegal move: {}{} to {}{} is occupied by a friendly piece.\n",
+      fromCol, fromRow, toCol, toRow);
+    return false;
+  }
+
+  int colStep = (toCol == fromCol) ? 0 : (toCol > fromCol ? 1 : -1);
+  int rowStep = (toRow == fromRow) ? 0 : (toRow > fromRow ? 1 : -1);
+
+  char c = fromCol + colStep;
+  int r = fromRow + rowStep;
+  while (c != toCol || r != toRow) {
+    if (board.get(c, r) != nullptr) {
+      std::cout << std::format("Illegal move: path from {}{} to {}{} is blocked.\n",
+        fromCol, fromRow, toCol, toRow);
+      return false;
+    }
+    c += colStep;
+    r += rowStep;
+  }
+
+  board.set(fromCol, fromRow, toCol, toRow, board.get(fromCol, fromRow));
+  return true;
 }
 
 PieceType Queen::getPieceType() const {
