f80276123ff145200edeb2a3b317bdd3136bcc17
Julian Sanchez <julianwsan@gmail.com>
2026-05-16 23:28:58 -0400

    defined new methods for en passant

--- include/ChessBoard.h
+++ include/ChessBoard.h
@@ -8,12 +8,19 @@ class Piece;
 class ChessBoard{
   private:
     Piece* board[8][8];
+    char enPassantCol = '\0';
+    int  enPassantRow = 0;
 
   public:
     ChessBoard();
     Piece* get(char col, int row);
     void set(char init_col, int init_row, char new_col, int new_row, Piece* p);
+    void clear(char col, int row);
     void printBoard(Color turn) const;
+    void setEnPassant(char col, int row);
+    void clearEnPassant();
+    char getEnPassantCol() const;
+    int  getEnPassantRow() const;
 };
 
 #endif
